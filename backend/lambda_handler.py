8615a17466c42849086412dd2d9d1109e429710f
adi-vc <adityaheritage@gmail.com>
2026-03-08 17:21:30 +0530

    delete files not needed now

--- backend/lambda_handler.py
+++
@@ -1,7 +0,0 @@
-"""Lambda handler for FastAPI application using Mangum."""
-
-from mangum import Mangum
-from app.main import app
-
-# Mangum adapter for AWS Lambda
-handler = Mangum(app, lifespan="off")
